00e9599bfe5e2829964181cfc9244f6c2f666267
simonajones <simonajones@gmail.com>
2022-10-25 09:01:21 +0100

    Tidy

--- src/components/forms/elements/formSwitcher.jsx
+++ src/components/forms/elements/formSwitcher.jsx
@@ -1,7 +1,9 @@
 import { Formik, Field, Form } from 'formik';
+import { useState } from 'react';
 
 const FormSwitcher = ({ options, callBack }) => {
 	//console.log('called with:', callBack);
+	const [option, setOption] = useState(null);
 
 	const choices = options.map((option, index) => (
 		<option key={index} value={index}>
@@ -11,29 +13,33 @@ const FormSwitcher = ({ options, callBack }) => {
 
 	const handleChange = (e) => {
 		const selected = e.target.value; // selected name
-		//console.log(options[e.target.value]);
+		//console.log(options[e.target.value].name);
+		setOption(options[e.target.value].name);
 		//console.log(selected);
 		callBack(selected);
 	};
 	return (
-		<Formik
-			initialValues={{
-				switcher: '',
-			}}
-		>
-			<Form>
-				<Field
-					className="mt-1 w-full rounded border py-2 px-3"
-					as="select"
-					id="switcher"
-					name="switcher"
-					onChange={handleChange}
-				>
-					<option value="">Select an option</option>
-					{choices}
-				</Field>
-			</Form>
-		</Formik>
+		<>
+			<Formik
+				initialValues={{
+					switcher: '',
+				}}
+			>
+				<Form>
+					<Field
+						className="mt-1 w-full rounded border py-2 px-3"
+						as="select"
+						id="switcher"
+						name="switcher"
+						onChange={handleChange}
+					>
+						<option value="">Select an option</option>
+						{choices}
+					</Field>
+				</Form>
+			</Formik>
+			{option && <p>{option}</p>}
+		</>
 	);
 };
 
