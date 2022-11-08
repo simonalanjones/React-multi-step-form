fdb9cb9e077d69d1356aa34b5ae3a5c2d720b2c6
simonajones <simonajones@gmail.com>
2022-11-08 22:18:02 +0000

    removed form selection as dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import Wizard from './components/Wizard';
 import Progress from './components/Progress';
-import FormSwitcher from './components/forms/elements/formSwitcher';
+//import FormSwitcher from './components/forms/elements/formSwitcher';
 import logo from './assets/axa-logo-0-2048x2048.png';
 //import Layout from './components/Layout';
 import FormSelector from './components/FormSelector';
+import Feedback from './Feedback';
 
 function App() {
   const [stepsData, setStepsData] = useState(null); // all the wizard steps
@@ -14,7 +15,15 @@ function App() {
   const [isSelecting, setIsSelecting] = useState(true);
   const [isUsingFeedback, setIsUsingFeedback] = useState(false);
 
-  const giveFeedback = () => {};
+  const giveFeedback = () => {
+    console.log('leave feedback');
+    setIsUsingFeedback(true);
+    // set-up stepData
+  };
+
+  const onSubmit = values => {
+    console.log('submit in App with values,', values);
+  };
 
   const callbackForm = stepsData => {
     setStepsData(stepsData);
@@ -35,30 +44,35 @@ function App() {
       <div className="flex h-screen flex-col">
         <div className="hidden bg-blue-800 py-4 sm:block"></div>
 
-        {isSelecting && <FormSelector callBack={callbackForm} />}
+        {isSelecting && !isUsingFeedback && (
+          <FormSelector callBack={callbackForm} />
+        )}
 
-        {stepsData && !isSelecting && (
-          <div className="flex flex-grow bg-slate-50 p-8">
-            <div className="mx-auto flex w-screen max-w-4xl flex-col">
-              <div className="flex-grow">
+        <div className="flex flex-grow bg-slate-50 p-8">
+          <div className="mx-auto flex w-screen max-w-4xl flex-col">
+            {isUsingFeedback && <Feedback />}
+            <div className="flex-grow">
+              {stepsData && !isSelecting && (
                 <div className="flex h-full justify-center">
-                  <div className="apx-6 w-64 py-12 lg:mr-16">
+                  <div className="w-64 py-12 lg:mr-16">
                     <Progress headings={headings} currentStep={stepNumber} />
                   </div>
                   <div className="w-full border bg-white">
                     <Wizard
                       stepsData={stepsData}
                       progressCallback={callbackProgress}
+                      onSubmit={onSubmit}
                     />
                   </div>
                 </div>
-              </div>
+              )}
             </div>
           </div>
-        )}
+        </div>
+
         <div className="h-8 bg-slate-50 text-center">
           {/* <img width="48" height="48" src={logo} alt="AXA logo" /> */}
-          the footer
+          the footer. <span onClick={() => giveFeedback()}>Leave feedback</span>
         </div>
       </div>
     </>
---
+++ src/MenuSelection.jsx
@@ -0,0 +1,7 @@
+import FormSelector from './components/FormSelector';
+
+const MenuSelection = () => {
+  return <FormSelector callBack={callbackForm} />;
+};
+
+export default MenuSelection;
