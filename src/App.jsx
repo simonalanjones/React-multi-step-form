32bc51f8375109e58d2109e523c36a7fcbde3a7a
simonajones <simonajones@gmail.com>
2022-11-08 08:37:25 +0000

    Form selection menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import Progress from './components/Progress';
 import FormSwitcher from './components/forms/elements/formSwitcher';
 import logo from './assets/axa-logo-0-2048x2048.png';
 //import Layout from './components/Layout';
+import FormSelector from './components/FormSelector';
 
 function App() {
   const [stepsData, setStepsData] = useState(null); // all the wizard steps
   const [stepNumber, setStepNumber] = useState(null);
   const [headings, setHeadings] = useState(null);
+
+  const [isSelecting, setIsSelecting] = useState(true);
+  const [isUsingFeedback, setIsUsingFeedback] = useState(false);
+
+  const giveFeedback = () => {};
 
   const callbackForm = stepsData => {
     setStepsData(stepsData);
@@ -17,6 +23,7 @@ function App() {
       headings.push(step.heading);
     });
     setHeadings(headings);
+    setIsSelecting(false);
   };
 
   const callbackProgress = stepNumber => {
@@ -26,12 +33,11 @@ function App() {
   return (
     <>
       <div className="flex h-screen flex-col">
-        <div className="hidden bg-indigo-800 py-4 sm:block">
-          <div className="mx-auto max-w-lg">
-            {<FormSwitcher callBack={callbackForm} />}
-          </div>
-        </div>
-        {stepsData && (
+        <div className="hidden bg-blue-800 py-4 sm:block"></div>
+
+        {isSelecting && <FormSelector callBack={callbackForm} />}
+
+        {stepsData && !isSelecting && (
           <div className="flex flex-grow bg-slate-50 p-8">
             <div className="mx-auto flex w-screen max-w-4xl flex-col">
               <div className="flex-grow">
