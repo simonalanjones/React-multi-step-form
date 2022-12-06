7ac05453b0f3a6b3c917c7c8b97935146b25a911
Simon Jones <simonajones@gmail.com>
2022-12-06 15:42:13 +0000

    Use custom hooks

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,66 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Wizard from './components/Wizard';
 import Progress from './components/Progress';
 import FormView from './components/views/Form.view';
+import useForm from './components/hooks/useForm';
 
 const Form = () => {
-  const [stepsData, setStepsData] = useState(null); // all the wizard steps
   const [stepNumber, setStepNumber] = useState(null);
-  const [headings, setHeadings] = useState(null);
-  const [formTitle, setFormTitle] = useState(null);
-
   const params = useParams();
   const navigate = useNavigate();
+  const { stepsData, headings, formTitle } = useForm(params.id);
 
-  async function fetchForm(filename) {
-    const response = await fetch(`../src/configs/${filename}.json`);
-    return await response.json();
-  }
-
-  async function fetchData() {
-    const response = await fetch('./../src/configs/index.json');
-    return await response.json();
-  }
-
-  async function findElement() {}
-
-  const getFilename = urlName =>
-    fetchData()
-      .then(formData => {
-        return formData
-          .find(product => product.forms.find(item => item.urlName === urlName))
-          .forms.find(item => item.urlName === urlName);
-      })
-      .then(form => {
-        console.log(form);
-        setFormTitle(form.name);
-        return form.file;
-        //console.log('found:', element);
-      })
-      .catch(error => {
-        console.log('error:', error);
-      });
-
-  useEffect(() => {
-    const filename = getFilename(params.id).then(filename => {
-      console.log(filename);
-      fetchForm(filename).then(formData => {
-        setStepsData(formData);
-        let headings = [];
-        formData.map(step => {
-          headings.push(step.heading);
-        });
-        setHeadings(headings);
-      });
-    });
-  }, []);
-
-  const callbackProgress = stepNumber => {
+  const onStepChange = (stepNumber) => {
     setStepNumber(stepNumber);
   };
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     console.log('you submitted', data);
   };
 
@@ -78,7 +33,7 @@ const Form = () => {
         wizard={
           <Wizard
             stepsData={stepsData}
-            progressCallback={callbackProgress}
+            progressCallback={onStepChange}
             onSubmit={onSubmit}
           />
         }
