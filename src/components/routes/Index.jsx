b34470cbfdbb11f332b34f0f18333718893dc308
Simon Jones <simonajones@gmail.com>
2022-12-06 15:41:15 +0000

    Custom hooks

--- src/components/routes/Index.jsx
+++ src/components/routes/Index.jsx
@@ -1,63 +1,20 @@
-import { useState, useEffect } from 'react';
 import IndexView from '../views/Index.view';
 import TeamList from '../TeamList';
 import FormList from '../FormList';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import useTeams from '../hooks/useTeams';
 
 const Index = () => {
-  const [teams, setTeams] = useState([]);
-  const [forms, setForms] = useState([]);
-  const params = useParams();
   const navigate = useNavigate();
+  const params = useParams();
+  const { teams, forms } = useTeams(params.urlName);
 
-  // 1st function: load the teams array
-  useEffect(() => {
-    fetchData().then(formData => {
-      //console.log(formData);
-      setTeams(formData);
-    });
-  }, []);
-
-  // 2nd function: set forms if team id given
-  // team id is param: urlName
-  useEffect(() => {
-    if (params.urlName && teams.length > 0) {
-      //console.log('here....');
-      const team = teams.find(
-        element => element.team.urlName === params.urlName
-        //
-      );
-      // check result found
-      if (team !== undefined) {
-        setForms(team.forms);
-      } else {
-        console.log('team name not found:', params.urlName);
-      }
-    }
-
-    // else no team set so add all forms into form list
-    if (!params.urlName && forms === []) {
-      fetchData().then(formData => {
-        const forms = formData.map(element => element.forms).flat();
-        console.log(forms);
-        setForms(forms);
-      });
-    }
-  });
-
-  // todo: have service to get teams/forms/ etc
-  async function fetchData() {
-    const response = await fetch('./../src/configs/index.json');
-    return await response.json();
-  }
-
-  const selectedTeam = id => {
-    //console.log(id);
+  const selectedTeam = (id) => {
     navigate(`/team/${id}`);
   };
 
-  const selectedForm = id => {
+  const selectedForm = (id) => {
     console.log(id);
     navigate(`/form/${id}`);
   };
