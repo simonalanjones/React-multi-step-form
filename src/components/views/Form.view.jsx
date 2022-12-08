5df3ede89a9a08011c72104be6cd83f1b1ac5723
Simon Jones <simonajones@gmail.com>
2022-12-08 19:48:27 +0000

    Better responsive layout

--- src/components/views/Form.view.jsx
+++ src/components/views/Form.view.jsx
@@ -1,54 +1,23 @@
+import axalogo from '../../../public/axa-logo.png';
+
 const FormView = ({ progress, wizard, onChangeHandler, title }) => {
   return (
-    <div className="bg-gray-50">
-      {/* background colour across entire page*/}
-      <div className="mx-auto flex max-w-6xl flex-row">
-        <div className="bg-gray-100">
-          <div className="flex h-screen flex-col items-center border-r-2 p-16">
-            {progress}
+    <>
+      <div className="flex h-screen flex-col md:bg-gray-50">
+        <div className="flex items-center justify-center border-b border-gray-200 py-4">
+          <div>
+            <img width="60" height="60" src={axalogo} />
           </div>
         </div>
 
-        <div className="ap-16 max-w-4xl bg-white">
-          <p>{wizard}</p>
+        <div className="mx-auto w-full max-w-4xl flex-grow pt-8 md:flex md:flex-nowrap">
+          <div className="mx-auto w-fit px-8">{progress}</div>
+          <div className="flex-grow px-8 pt-8 md:pt-0 md:pr-0">{wizard}</div>
         </div>
+
+        <div className="bg-slate-50 text-center">BOTTOM NAV</div>
       </div>
-    </div>
-
-    // bring in header!
-    // <div className="flex h-screen flex-col">
-    //   <div className="hidden bg-blue-800 py-4 sm:block">
-    //     <div className="flex items-center justify-center">
-    //       <div className="pr-4">
-    //         <span className="text-center text-lg text-white">{title}</span>
-    //       </div>
-    //       <div>
-    //         <button
-    //           onClick={onChangeHandler}
-    //           type="button"
-    //           className="rounded-lg border border-transparent bg-gray-200/90 py-2 px-4 text-sm font-medium text-gray-800 hover:bg-gray-100 focus:outline-none"
-    //         >
-    //           Change
-    //         </button>
-    //       </div>
-    //     </div>
-    //   </div>
-
-    //   <div className="flex flex-grow bg-slate-50 p-8">
-    //     <div className="mx-auto flex w-screen max-w-4xl flex-col">
-    //       <div className="flex-grow">
-    //         <div className="flex h-full justify-center">
-    //           <div className="w-64 py-12 lg:mr-16">{progress}</div>
-    //           <div className="w-full border bg-white">{wizard}</div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   {/* // bring in footer! */}
-    //   <div className="h-8 bg-slate-50 text-center">
-    //     the footer. <span onClick={onChangeHandler}>Leave feedback</span>
-    //   </div>
-    // </div>
+    </>
   );
 };
 
--- src/components/views/Index.view.jsx
+++ src/components/views/Index.view.jsx
@@ -1,14 +1,19 @@
+import axalogo from '../../../public/axa-logo.png';
+
 const IndexView = ({ teams, forms, feedbackLink }) => {
-  console.log('in indexview');
+  //console.log('in indexview');
   return (
     <>
-      <div className="abg-slate-50 container flex h-screen flex-col">
-        <div className="bg-slate-50 text-center">TOP NAV</div>
+      <div className=" flex h-screen flex-col">
+        <div className="flex items-center justify-center border-b border-gray-200 py-4">
+          <div>
+            <img width="60" height="60" src={axalogo} />
+          </div>
+        </div>
 
-        <div className="abg-green-500 mx-auto max-w-5xl flex-grow md:flex">
-          <div className="aborder abg-slate-100 p-4 pt-8">{teams}</div>
-
-          <div className="abg-slate-700 p-4">{forms}</div>
+        <div className="mx-auto w-full max-w-4xl flex-grow pt-8 md:flex md:flex-nowrap">
+          <div className="mx-auto w-fit px-8">{teams}</div>
+          <div className="flex-grow px-8 pt-8 md:pt-0 md:pr-0">{forms}</div>
         </div>
         <div className="bg-slate-50 text-center">BOTTOM NAV</div>
       </div>
