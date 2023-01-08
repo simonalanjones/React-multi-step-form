c5a7b6fecc3f55b56f421a90dacd0caf40435260
Simon Jones <simonajones@gmail.com>
2023-01-08 12:15:55 +0000

    Arrow syntax improvement

--- src/components/forms/elements/requiredMark.jsx
+++ src/components/forms/elements/requiredMark.jsx
@@ -1,5 +1,3 @@
-const RequiredMark = () => {
-  return <span className="ml-1 text-sm text-red-600">*</span>;
-};
+const RequiredMark = () => <span className="ml-1 text-sm text-red-600">*</span>;
 
 export default RequiredMark;
