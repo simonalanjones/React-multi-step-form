8900840f9d84f48f36bb6b8e4492939a44526d4d
Simon Jones <simonajones@gmail.com>
2023-01-08 12:17:58 +0000

    Style changes

--- src/components/forms/elements/nextButton.jsx
+++ src/components/forms/elements/nextButton.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
-const NextButton = ({ label = 'Next' }) => {
-  //const NextButton = React.forwardRef((props, ref) => {
-  return (
-    <button
-      //ref={ref}
-      type="submit"
-      className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-    >
-      {label}
-    </button>
-  );
-};
+const NextButton = ({ label = 'Next' }) => (
+  <button
+    type="submit"
+    className="rounded-md border border-transparent bg-blue-700 
+      py-2 px-4 text-sm font-medium leading-5 text-white shadow-sm
+      focus:outline-none focus:ring-2 focus:ring-blue-700 
+      focus:ring-offset-2 hover:bg-blue-800"
+  >
+    {label}
+  </button>
+);
 
 export default NextButton;
--- src/components/forms/elements/previousButton.jsx
+++ src/components/forms/elements/previousButton.jsx
@@ -4,7 +4,16 @@ const PreviousButton = React.forwardRef((props, ref) => {
   return (
     <button
       type="button"
-      className="inline-flex justify-center rounded-md border border-gray-300 bg-gray-200 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      className="rounded-md border 
+      border-gray-300 
+      bg-white 
+      py-2 px-3 
+      text-sm font-medium leading-5 text-gray-700 
+      shadow-sm 
+      focus:outline-none 
+      focus:ring-2 
+      focus:ring-gray-500 
+      focus:ring-offset-2 hover:bg-gray-50"
       onClick={props.onClick}
     >
       Previous
