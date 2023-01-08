597508cffcffe0bba4f287d828ecdfb7c79e4d3b
Simon Jones <simonajones@gmail.com>
2023-01-08 12:14:08 +0000

    Fix for single item array

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,24 +2,33 @@ import React from 'react';
 import AbstractFormElement from './forms/elements/abstractFormElement';
 
 const Container = ({ element, index }) => {
-  if (Array.isArray(element)) {
+  //
+  const SingleColumn = ({ item }, key) => {
     return (
-      <div className={`mb-2 grid gap-6 pb-4 md:grid-cols-2`} key={index}>
-        {element.map((item, index) => {
-          return (
-            <div key={index}>
-              <AbstractFormElement item={item} />
-            </div>
-          );
-        })}
+      <div className="w-full pb-6" key={key}>
+        <AbstractFormElement item={item} />
       </div>
+    );
+  };
+  //
+  const MultiColumn = ({ items }) => {
+    return (
+      <div className="gap-6 md:flex" key={index}>
+        {items.map((item, index) => (
+          <SingleColumn item={item} key={index} />
+        ))}
+      </div>
+    );
+  };
+
+  if (Array.isArray(element)) {
+    return element.length === 1 ? (
+      <SingleColumn item={element[0]} key="1" />
+    ) : (
+      <MultiColumn items={element} />
     );
   } else {
-    return (
-      <div className={`columns-1 gap-6 pb-5`} key={index}>
-        <AbstractFormElement item={element} />
-      </div>
-    );
+    return <SingleColumn item={element} key="1" />;
   }
 };
 
