a819c6d341bd4fceb06fe556a5e621c2aada76a5
Simon Jones <simonajones@gmail.com>
2023-01-11 00:06:34 +0000

    Addded 'When' validation

--- src/yupSchemaCreator.js
+++ src/yupSchemaCreator.js
@@ -1,3 +1,23 @@
+// import * as yup from 'yup';
+
+// export function createYupSchema(schema, config) {
+//   const { id, validationType, validations = [] } = config;
+//   if (!yup[validationType]) {
+//     return schema;
+//   }
+//   let validator = yup[validationType]();
+//   validations.forEach(validation => {
+//     const { params, type } = validation;
+//     if (!validator[type]) {
+//       return;
+//     }
+//     //console.log(type, params);
+//     validator = validator[type](...params);
+//   });
+//   schema[id] = validator;
+//   return schema;
+// }
+
 import * as yup from 'yup';
 
 export function createYupSchema(schema, config) {
@@ -6,13 +26,26 @@ export function createYupSchema(schema, config) {
     return schema;
   }
   let validator = yup[validationType]();
-  validations.forEach(validation => {
+  validations.forEach((validation) => {
     const { params, type } = validation;
     if (!validator[type]) {
       return;
     }
-    //console.log(type, params);
-    validator = validator[type](...params);
+    if (type === 'when') {
+      const { is, then, otherwise } = params[1];
+      let whenParams = {};
+      whenParams.is = is;
+      whenParams.then = (schema) => schema[then[0].type](...then[0].params);
+
+      if (otherwise) {
+        whenParams.otherwise = (schema) =>
+          schema[otherwise[0].type](...otherwise[0].params);
+      }
+
+      validator = validator['when'](params[0], whenParams);
+    } else {
+      validator = validator[type](...params);
+    }
   });
   schema[id] = validator;
   return schema;
