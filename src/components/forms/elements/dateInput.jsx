1f951eaae9388d1bb95f2a5ef6e98f93c9b06ef8
Simon Jones <simonajones@gmail.com>
2023-01-11 00:08:46 +0000

    Syntax tweaks

--- src/components/forms/elements/dateInput.jsx
+++ src/components/forms/elements/dateInput.jsx
@@ -21,7 +21,7 @@ const DateInput = ({ label, ...props }) => {
         id={field.name}
         name={field.name}
         value={field.value || ''}
-      ></Field>
+      />
 
       {props.labelHelper && <LabelHelper helperText={props.labelHelper} />}
       {meta.touched && meta.error && <ErrorText error={meta.error} />}
