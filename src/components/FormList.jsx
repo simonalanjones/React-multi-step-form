f7a360d359c1aeea918c6f3ee527c94075081dd2
Simon Jones <simonajones@gmail.com>
2023-01-30 20:56:48 +0000

    Use grid

--- src/components/FormList.jsx
+++ src/components/FormList.jsx
@@ -3,16 +3,18 @@ import FormListItem from './FormListItem';
 const FormList = ({ forms, selectedCallback }) => {
   const navigate = useNavigate();
 
-  if (Array.isArray(forms)) {
-    return forms.map((option, index) => (
-      <FormListItem
-        key={index}
-        name={option.name}
-        description={option.description}
-        url={() => navigate(`/form/${option.urlName}`)}
-      />
-    ));
-  }
+  return (
+    <div className="mb-6 grid grid-cols-2 gap-6">
+      {forms.map((option, index) => (
+        <FormListItem
+          key={index}
+          name={option.name}
+          description={option.description}
+          url={() => navigate(`/form/${option.urlName}`)}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default FormList;
