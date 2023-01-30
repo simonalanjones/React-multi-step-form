1aec28cb63438469b9f51290f84d0a5583c6375c
Simon Jones <simonajones@gmail.com>
2023-01-30 21:02:34 +0000

    Tidying code

--- src/components/views/Feedback.view.jsx
+++ src/components/views/Feedback.view.jsx
@@ -1,45 +1,27 @@
-const FeedbackView = props => {
+import axahealthlogo from '../../../public/axa-health_logo_solid_rgb.webp';
+import { Link } from 'react-router-dom';
+const FeedbackView = (props) => {
   return (
-    <>
-      <div className="flex h-screen flex-col">
-        <div className="hidden bg-blue-800 py-4 sm:block">
-          <div className="flex items-center justify-center">
-            <div className="pr-4">
-              <span className="text-center text-lg text-white">
-                Leave feedback
-              </span>
-            </div>
-            <div>
-              <button
-                onClick={props.cancelFeedback}
-                type="button"
-                className="rounded-lg border border-transparent bg-gray-200/90 py-2 px-4 text-sm font-medium text-gray-800 focus:outline-none hover:bg-gray-100"
-              >
-                Change
-              </button>
-            </div>
-          </div>
+    <div className="flex h-screen flex-col">
+      <div className="mx-auto w-full max-w-5xl items-end border-0 pt-4 pb-4 md:flex md:flex-nowrap">
+        <div className="px-8">
+          <img width="130" src={axahealthlogo} />
         </div>
-
-        <div className="flex flex-grow bg-slate-50 p-8">
-          <div className="mx-auto flex w-screen max-w-4xl flex-col">
-            <div className="mx-auto min-w-fit border bg-white">
-              {props.children}
-            </div>
-          </div>
+        <div className="flex grow justify-end gap-x-12 text-base font-bold uppercase">
+          <p className="_hover:underline text-blue-800 decoration-blue-800 decoration-0 underline-offset-8 hover:cursor-pointer ">
+            <Link to="/">provider management referral forms</Link>
+          </p>
+          <p className="text-blue-800 underline decoration-blue-800 decoration-0 underline-offset-8">
+            feedback
+          </p>
         </div>
-        {/* // bring in footer! */}
-        <div className="h-8 bg-slate-50 text-center">the footer.</div>
       </div>
-
-      {/* <div className="flex flex-grow bg-slate-50 p-8">
-        <div className="mx-auto flex w-screen max-w-4xl flex-col">
-          <div className="mx-auto min-w-fit border bg-white">
-            {props.children}
-          </div>
+      <div className="mx-auto mb-8 w-full max-w-4xl flex-grow  pt-8 md:flex md:flex-nowrap">
+        <div className="flex-grow px-8 pt-8 md:pt-0 md:pr-0">
+          {props.children}
         </div>
-      </div> */}
-    </>
+      </div>
+    </div>
   );
 };
 
